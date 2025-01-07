0d91b97242921928dfd7505c98e37f4d7fee0e20
LuigiDotSRC <luigi.terashima@gmail.com>
2025-01-07 13:42:13 -0500

    neetcode 9/9 arrays+hashing

---
+++ 128-LongestConsequtiveSequence.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# sequences intuition
+# using hash sets for O(1) lookup
+
+class Solution:
+    def longestConsequtive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
