391c652edfc73239a6de071e0bc964a161b5430f
LuigiDotSRC <luigi.terashima@gmail.com>
2025-01-13 12:37:42 -0500

    neetcode 5/5 twopointers

--- 42-TrappingRainWater.py
+++ 42-TrappingRainWater.py
@@ -2,6 +2,21 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
-        res, max = 0, 0
+        res, lmax, rmax = 0, 0, 0
         while l < r:
-            pass
+            if height[l] >= lmax:
+                lmax = height[l]
+            else:
+                res += lmax - height[l]
+            
+            if height[r] >= rmax:
+                rmax = height[r]
+            else:
+                res += rmax - height[r]
+            
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        
+        return res
