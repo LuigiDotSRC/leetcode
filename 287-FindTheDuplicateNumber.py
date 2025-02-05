6080c014c60128d87e0d5b427aa85c608eb4e269
LuigiDotSRC <luigi.terashima@gmail.com>
2025-02-05 12:36:39 -0500

    neetcode 8/11 linked list

---
+++ 287-FindTheDuplicateNumber.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        visited = set()
+        for n in nums:
+            if n in visited:
+                return n
+            visited.add(n)
+        
